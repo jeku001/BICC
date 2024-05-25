0f1800b28ae69cf89bb4d94766f309d77ad75f9f
anmc01 <kornobisfranek@gmail.com>
2024-05-25 14:02:17 +0200

    predykcja metodami stl i tbats
    predykcja z wykresami trendu, wahań sezonowych i reszt, wykresem danych wraz z przewidzianymi danymi (z przedziałami ufności), wykresem porównującym ostatnie dwa lata z przewidzianymi nastepnymi 2 latami, obliczenie błędu dla ostatnich 2 lat vs przewidzianych 2 lat - wszystko dla przypadku z 2020 i bez 2020 - bez 2020 daje lepsze wyniki

---
+++ stl_tbats.r
@@ -0,0 +1,101 @@
+# install.packages('forecast')
+library(forecast)
+
+# Funkcje bledu -----------------------------------------------------------
+
+MAPE.error <- function(x, y) {
+  result <- mean(abs((x-y)/x)) * 100
+  return(result)
+}
+
+RMSE.error <- function(x, y) {
+  result <- sqrt((sum(x - y)^2)/length(x))
+  return(result)
+}
+
+# Z 2020 ------------------------------------------------------------------
+
+# Podzial danych ----------------------------------------------------------
+
+df.konstr <- head(df$Wskaznik, n = 12 * 20) # czesc konstrukcyjna
+df.test <- tail(df$Wskaznik, n = 12 * 2) # czesc testowa
+
+# Metoda STL --------------------------------------------------------------
+
+ts_stl <- ts(df$Wskaznik, frequency = 12) # utworzenie szeregu czasowego dla stl
+model.ts_stl <- stl(ts_stl, s.window = "periodic") #obliczenie trendu, wahan sezonowych i reszt
+autoplot(model.ts_stl)
+
+model.ts_stl.forecast <- forecast(model.ts_stl, h = 24) # predykcja dla kolejnych dwóch lat wraz z granicami
+                                                        # przedzialów ufnosci dla poziomów ufnosci 80% i 95%
+autoplot(model.ts_stl.forecast) # wykres danych wraz z przewidzianymi wartosciami wskaznika
+                                # ciemnoniebieskie pole to przedzial ufnosci 80%, jasnoniebieskie - 95%
+model.ts_stl.forecast$mean
+
+ts_stl.predict <- predict(model.ts_stl.forecast, df.test)
+ts_stl.test_vs_predicted <- data.frame(df.test, model.ts_stl.forecast$mean) # tabela z danymi z ostatnich 2 lat zestawiona
+                                                                            # z przewidzianymi danymi na nastepne 2 lata
+matplot(ts_stl.test_vs_predicted, type = 'l', lty = 1:2, col = 1:2)
+# wykres danych z ostatnich 2 lat (na czarno) vs przewidzianych danych na kolejne 2 lata (na czerwono)
+
+MAPE.error(ts_stl.test_vs_predicted$df.test, ts_stl.test_vs_predicted$model.ts_stl.forecast.mean) # 3.323
+RMSE.error(ts_stl.test_vs_predicted$df.test, ts_stl.test_vs_predicted$model.ts_stl.forecast.mean) # 16.506
+
+# Metoda TBATS ------------------------------------------------------------
+
+ts_tbats <- msts(df$Wskaznik, seasonal.periods = 12)
+model.ts_tbats <- tbats(ts_tbats)
+plot(model.ts_tbats)
+
+model.ts_tbats.forecast <- forecast(model.ts_tbats, h = 24)
+plot(model.ts_tbats.forecast)
+model.ts_tbats.forecast$mean
+
+ts_tbats.predict <- predict(model.ts_tbats.forecast, df.test)
+ts_tbats.test_vs_predicted <- data.frame(df.test, model.ts_tbats.forecast$mean)
+matplot(ts_tbats.test_vs_predicted, type = 'l', lty = 1:2, col = 1:2)
+
+MAPE.error(ts_tbats.test_vs_predicted$df.test, ts_tbats.test_vs_predicted$model.ts_tbats.forecast.mean) # 2.085
+RMSE.error(ts_tbats.test_vs_predicted$df.test, ts_tbats.test_vs_predicted$model.ts_tbats.forecast.mean) # 6.855
+
+# BEZ 2020 ----------------------------------------------------------------
+
+# Podzial danych 2---------------------------------------------------------
+
+df_no_2020
+df_no_2020.konstr <- head(df_no_2020$Wskaznik, n = 12 * 19)
+df_no_2020.test <- tail(df_no_2020$Wskaznik, n = 12 * 2)
+
+# Metoda STL 2-------------------------------------------------------------
+
+ts_stl2 <- ts(df_no_2020$Wskaznik, frequency = 12)
+model.ts_stl2 <- stl(ts_stl2, s.window = "periodic")
+autoplot(model.ts_stl2)
+
+model.ts_stl2.forecast <- forecast(model.ts_stl2, h = 24)
+autoplot(model.ts_stl2.forecast)
+model.ts_stl2.forecast$mean
+
+ts_stl2.predict <- predict(model.ts_stl2.forecast, df_no_2020.test)
+ts_stl2.test_vs_predicted <- data.frame(df_no_2020.test, model.ts_stl2.forecast$mean)
+matplot(ts_stl2.test_vs_predicted, type = 'l', lty = 1:2, col = 1:2)
+
+MAPE.error(ts_stl2.test_vs_predicted$df_no_2020.test, ts_stl2.test_vs_predicted$model.ts_stl2.forecast.mean) # 2.209
+RMSE.error(ts_stl2.test_vs_predicted$df_no_2020.test, ts_stl2.test_vs_predicted$model.ts_stl2.forecast.mean) # 10.667
+
+# Metoda TBATS 2-----------------------------------------------------------
+
+ts_tbats2 <- msts(df_no_2020$Wskaznik, seasonal.periods = 12)
+model.ts_tbats2 <- tbats(ts_tbats2)
+plot(model.ts_tbats2)
+
+model.ts_tbats2.forecast <- forecast(model.ts_tbats2, h = 24)
+plot(model.ts_tbats2.forecast)
+model.ts_tbats2.forecast$mean
+
+ts_tbats2.predict <- predict(model.ts_tbats2.forecast, df_no_2020.test)
+ts_tbats2.test_vs_predicted <- data.frame(df_no_2020.test, model.ts_tbats2.forecast$mean)
+matplot(ts_tbats2.test_vs_predicted, type = 'l', lty = 1:2, col = 1:2)
+
+MAPE.error(ts_tbats2.test_vs_predicted$df_no_2020.test, ts_tbats2.test_vs_predicted$model.ts_tbats2.forecast.mean) # 1.169
+RMSE.error(ts_tbats2.test_vs_predicted$df_no_2020.test, ts_tbats2.test_vs_predicted$model.ts_tbats2.forecast.mean) # 3.977
